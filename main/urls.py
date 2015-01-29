29c7ac04287f259d7d55d5eda2d2d4153ad46867
Michael Gilbert <drumz4jah@yahoo.com>
2015-01-28 20:41:48 -0700

    Added Location page (barebones)

--- main/urls.py
+++ main/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
     # performance detail: performance/1234/some_descriptive_text_for_useability_that_doesnt matter
     url(r'^performance/(?P<id>\d+)/(?P<slug>.+)', 'main.views.performance.detail',
         name='performance.id.slug'),
+	url(r'^location$', 'main.views.index.location', name='location'),
     # admin
     url(r'^admin/', include(admin.site.urls)),
 )
--- main/views/index.py
+++ main/views/index.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html', {})
+	
+# Putting the view for "location" here because it I don't think making an
+# app for it is worthwhile, as there is only a single page
+def location(request):
+	return render(request, 'location.html', {})
