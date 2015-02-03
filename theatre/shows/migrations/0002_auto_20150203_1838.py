38d277a1bb136d2c2b0b14c65e9a17b3209f060c
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-02-03 12:11:56 -0700

    had a typo in the migration, --fake back to 0001 and then reapplied 0002 with fix

--- theatre/shows/migrations/0002_auto_20150203_1838.py
+++ theatre/shows/migrations/0002_auto_20150203_1838.py
@@ -5,32 +5,32 @@ from django.db import models, migrations
 import datetime
 
 
-def initial_data(apps):
+def initial_data(apps, schema_editor):
     data = [
         {
             "name": "The Good, the Bad and the Ugly",
-            "data": datetime.date(2015, 2, 7),
+            "date": datetime.date(2015, 2, 7),
             "description": "Spaghetti Western starring Clint Eastwood",
             "length": 177,
             "price": 17.99
         },
         {
             "name": "The Godfather",
-            "data": datetime.date(2015, 2, 12),
+            "date": datetime.date(2015, 2, 12),
             "description": "Control of an organized crime empire is given to a reluctant son",
             "length": 175,
             "price": 17.99
         },
         {
             "name": "12 Angry Men",
-            "data": datetime.date(2015, 2, 17),
+            "date": datetime.date(2015, 2, 17),
             "description": "Behind the closed doors of a jury room",
             "length": 96,
             "price": 17.99
         },
         {
             "name": "The Matrix",
-            "data": datetime.date(2015, 2, 18),
+            "date": datetime.date(2015, 2, 18),
             "description": "A hacker rebels against his controllers.",
             "length": 136,
             "price": 17.99
@@ -51,4 +51,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(initial_data)
+
     ]
