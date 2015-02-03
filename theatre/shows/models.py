8b831a3fee9231bf62234b97775fb39961c660e5
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-02-03 13:10:30 -0700

    modified shows, all page and model

--- theatre/shows/models.py
+++ theatre/shows/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Show(models.Model):
     name = models.CharField(max_length=200)
     length = models.IntegerField()
--- theatre/shows/views.py
+++ theatre/shows/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from models import Show
 
 def all(request):
-    return render(request, "shows/all.html", {})
+    shows = Show.objects.all()
+    context = {"shows": shows}
+    return render(request, "shows/all.html", context)
 
 
 def detail(request, id, slug=None):
-    return render(request, "shows/detail.html", {})
+    show = get_object_or_404(Show, pk=id)
+    context = {"show": show}
+    return render(request, "shows/detail.html", context)
