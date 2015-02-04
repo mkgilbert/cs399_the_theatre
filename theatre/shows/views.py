888d44986b74c084217e5478d26b7cf26fb83d0b
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-02-04 15:12:36 -0700

    added ticket page

--- theatre/shows/views.py
+++ theatre/shows/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from models import Show
+from models import Show, Ticket
 
 
 def all(request):
@@ -12,3 +12,12 @@ def detail(request, id, slug=None):
     show = get_object_or_404(Show, pk=id)
     context = {"show": show, "tickets": show.tickets.all()}
     return render(request, "shows/detail.html", context)
+
+
+def ticket(request,show_id, ticket_id):
+
+    context = {
+        "ticket": get_object_or_404(Ticket, pk=ticket_id),
+        "show": get_object_or_404(Show, pk=show_id)
+    }
+    return render(request, "shows/ticket.html", context)
